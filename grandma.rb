7e546345cabe0f13168c21b4cf111cbe25b65359
Allen Phillip Pottinger <allenpottinger@gmail.com>
2019-12-22 00:02:36 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,10 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+speak_to_grandma
+If "Hello GRANDMA".downcase
+  puts "HUH?! SPEAK UP, SONNY!".upcase
+elsif "I said 'Hello Grandma".downcase
+  puts "HUH?! SPEAK UP, SONNY!".upcase
+else "I LOVE YOU GRANDMA!".upcase
+  puts "I LOVE YOU TOO PUMPKIN"
